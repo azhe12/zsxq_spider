f18d4e83b54b2ba781511de5605588fc275d5ce3
刘远哲 <yuanzheliu@tencent.com>
2019-09-30 17:23:30 +0800

    save topic to mysql

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
+//./main -startTime="2017-04-09T23:44:19.517+0800" -endTime="2010-04-09T23:44:18.517+0800"
+
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"io"
@@ -14,10 +17,12 @@ import (
 	"strconv"
 	"sync"
 	"time"
-	"models"
+	//"models"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 const PULL_DELAY = 100 //每次拉取一个page之后，延迟一段时间(单位毫秒)，避免反爬虫机制
+//const PULL_DELAY = 10000 //每次拉取一个page之后，延迟一段时间(单位毫秒)，避免反爬虫机制
 const PAGE_SIZE = 20
 //const GROUP_URL_FMT = "https://api.zsxq.com/v1.10/groups/%d/topics?scope=%s&count=%d&end_time=%s"  //其中%s是星球GROUP_ID, 如水库
 const GROUP_URL_PREFIX = "https://api.zsxq.com/v1.10/groups/%d/topics?"  //其中%s是星球GROUP_ID, 如水库
@@ -30,9 +35,11 @@ const TOPIC_FILE = "topics.txt"
 const IMAGE_DIR = "images"
 const IMAGE_PREFIX = "large_"
 const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
-const MOST_LONG_LONG_AGO = "2006-01-02T15:04:05.000+08000"
+const MOST_LONG_LONG_AGO = "2006-01-02T15:04:05.000+0800"
 const TOPIC_SAVE_TYPE_BIT_FILE = 1   //bitmap, 0位 存储topics到文件
 const TOPIC_SAVE_TYPE_BIT_MYSQL = 2   //bitmap, 1位 存储topics到mysql
+const TIME_ZONE_SHANGHAI 	= "Asia/Shanghai"
+const TIME_ZONE_0 			= ""
 
 type Owner struct {
 	User_id uint64 	`json:"user_id"`  //提问者id
@@ -67,7 +74,7 @@ type Answer struct {
 }
 
 type ShowComment struct {
-	Comment_id uint64 	`json:"comment_id"`
+	Comment_id int64 	`json:"comment_id"`
 	Create_time string 	`json:"create_time"`
 	Owner Owner 		`json:"owner"`
 	Text string 		`json:"text"` //评论内容
@@ -75,13 +82,12 @@ type ShowComment struct {
 
 //话题
 type Topic struct {
-	Topic_id uint64 	`json:"comment_id"`  //话题ID
 	Question Question 	`json:"question"` //提问
 	Answer Answer 		`json:"answer"` //回答
 	Show_comments []ShowComment 	`json:"show_comments"` //评论列表
-	Likes_count uint64 	`json:"likes_count"` //点赞数
-	Comments_count uint64 	`json:"comments_count"` //评论数
-	Reading_count uint64 `json:"reading_count"` //阅读数
+	Likes_count int64 	`json:"likes_count"` //点赞数
+	Comments_count int64 	`json:"comments_count"` //评论数
+	Reading_count int64 `json:"reading_count"` //阅读数
 	Create_time string 	`json:"create_time"` //创建时间2019-09-11T00:16:13.099+0800
 }
 
@@ -92,6 +98,38 @@ type RespData struct {
 type Resp struct {
 	Succeeded bool `json:"succeeded"`
 	Resp_data RespData `json:"resp_data"`
+}
+
+
+type Like struct {
+	ID        int    `gorm:"primary_key"`
+	Ip        string `gorm:"type:varchar(20);not null;index:ip_idx"`
+	Ua        string `gorm:"type:varchar(256);not null;"`
+	Title     string `gorm:"type:varchar(128);not null;index:title_idx"`
+	Hash      uint64 `gorm:"unique_index:hash_idx;"`
+	CreatedAt time.Time
+}
+/*
+CREATE TABLE zsxq.`topics` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `group_id` int(11) NOT NULL DEFAULT '0' COMMENT '星球id',
+    `topic_id` int(11) NOT NULL DEFAULT '0' COMMENT 'topic id',
+    `topic_content` varchar(65535) NOT NULL DEFAULT '' COMMENT '原始topic内容',
+    `topicTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'topic时间',
+    `insert_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '拉取时间',
+    `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_group_topic_id`(`group_id`, `topic_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='知识星球topic';
+*/
+
+type Topics struct {
+	ID 				int64 		`gorm:"primary_key"`
+	GroupId 		int64		`gorm:"type:bigint(20);not null;unique_index:idx_group_time_id;"`
+	TopicContent 	string 		`gorm:"type:text;not null;"`
+	UpdateAt 		time.Time	`gorm:"type:timestamp"`
+	TopicTime 		time.Time 	`gorm:"type:datetime(3);unique_index:idx_group_time_id;"`
+	CreateAt 		time.Time	`gorm:"type:timestamp"`
 }
 
 
@@ -124,19 +162,31 @@ func NewJsonStruct() *JsonStruct {
 var db *gorm.DB
 var topicSaveType int
 
+var (
+	g_startTime string
+	g_endTime string
+)
+
 func init() {
 	var err error
 	//链接mysql
-	db, err = gorm.Open("mysql", "root:900616/zsxq?charset=utf8&parseTime=True&loc=Local")
+	db, err = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/zsxq?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
-	if db.HasTable(models.Topics{}) {
+	if db.HasTable(Topics{}) {
+		fmt.Printf("zsxq.topics already exist\n")
+	} else {
+		fmt.Printf("zsxq.topics not exist, create!\n")
 		//判断表是否存在, 不存在就创建
-		db.CreateTable(models.Topics{})
+		db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci").CreateTable(Topics{})
 	}
 
 	topicSaveType = TOPIC_SAVE_TYPE_BIT_MYSQL
+
+	//startTime比endTime大，比如startTime="2017-04-30T15:49:11.423+0800" endTime="2017-04-30T14:49:11.423+0800"
+	flag.StringVar(&g_startTime, "startTime", "", "pull start time")
+	flag.StringVar(&g_endTime, "endTime", "", "pull end time")
 }
 
 //读取cookie json文件
@@ -166,16 +216,46 @@ func getParseParam(param string) string {
 	return url.PathEscape(param)
 }
 
+//func timeToStr(t time.Time, timeZone string) (string) {
+//	format := "2006-01-02T15:04:05.000+0800"
+//	//time.ParseInLocation("2006-01-02 15:04:05", t, time.Local)
+//	//var cstSh, _ = time.LoadLocation("Asia/Shanghai") //上海
+//	var cstSh, _ = time.LoadLocation(timeZone) //0时区
+//	return t.In(cstSh).Format(format)
+//}
+//
+//func strToTime(s string, timeZone string) (time.Time) {
+//	format := "2006-01-02T15:04:05.000+0800"
+//	//t, _ := time.Parse(format, s)
+//	var cst0, _ = time.LoadLocation(timeZone) //0时区
+//	t, _ := time.ParseInLocation(format, s, cst0)
+//	return t
+//}
+
 func timeToStr(t time.Time) (string) {
 	format := "2006-01-02T15:04:05.000+0800"
+	//time.ParseInLocation("2006-01-02 15:04:05", t, time.Local)
+	//var cstSh, _ = time.LoadLocation("Asia/Shanghai") //上海
+	//var cstSh, _ = time.LoadLocation(timeZone) //0时区
 	return t.Format(format)
 }
 
 func strToTime(s string) (time.Time) {
 	format := "2006-01-02T15:04:05.000+0800"
 	t, _ := time.Parse(format, s)
+	//var cst0, _ = time.LoadLocation(timeZone) //0时区
+	//t, _ := time.ParseInLocation(format, s, cst0)
 	return t
 }
+
+func strToTimeUtf8(s string) (time.Time) {
+	format := "2006-01-02T15:04:05.000+0800"
+	//t, _ := time.Parse(format, s)
+	var cst0, _ = time.LoadLocation(TIME_ZONE_SHANGHAI) //0时区
+	t, _ := time.ParseInLocation(format, s, cst0)
+	return t
+}
+
 
 //cookie 转字符串
 func convCookieToStr(cookies []Cookie) (string){
@@ -225,10 +305,10 @@ func httpGetWithCookie(urlStr string) []byte {
 
 
 //http api获取topic
-func getOnePageTopic(groupId int, endTime time.Time, pageSize int) ([]Topic) {
-	format := "2006-01-02T15:04:05.000+0800"
-	endTimeStr := endTime.Format(format)
-
+func getOnePageTopic(groupId int64, endTime time.Time, pageSize int) ([]Topic) {
+	//format := "2006-01-02T15:04:05.000+0000"
+	//endTimeStr := endTime.Format(format)
+	endTimeStr := timeToStr(endTime)
 
 	//urlVal := fmt.Sprintf(GROUP_URL_FMT, GROUP_ID_SHUIKU, SCOPE, pageSize, endTimeStr)
 	//fmt.Printf("urlval=%v", urlVal)
@@ -349,16 +429,30 @@ func saveTopicToFile(topics []Topic, filename string, imageDir string) {
 	}
 }
 
-func saveTopicToMysql(topics []Topic) {
+func saveTopicToMysql(groupId int64, topics []Topic) {
+	for i := 0; i < len(topics); i ++ {
+		topicJsonContent, _ := json.Marshal(topics[i])
+
+		topic := Topics{
+			GroupId: 		groupId,
+			TopicTime:		strToTimeUtf8(topics[i].Create_time),
+			TopicContent: 	string(topicJsonContent[:]),
+		}
+		fmt.Printf("---------------topic %v\n", topic)
+		err := db.Create(&topic).Error
+		if err != nil {
+			fmt.Printf("saveTopicToMysql | occur err:%v", err)
+		}
+	}
 
 }
 
 //写入topic到文件
-func saveTopic(waitGroup *sync.WaitGroup, topics []Topic, filename string, imageDir string) () {
+func saveTopic(waitGroup *sync.WaitGroup, groupId int64, topics []Topic, filename string, imageDir string) () {
 
 	//保存到mysql
 	if topicSaveType | TOPIC_SAVE_TYPE_BIT_MYSQL != 0 {
-		saveTopicToMysql(topics)
+		saveTopicToMysql(groupId, topics)
 	}
 	//保存到文件
 	if topicSaveType | TOPIC_SAVE_TYPE_BIT_FILE != 0  {
@@ -376,11 +470,12 @@ func getPageInfoFromTopics(topics []Topic) (time.Time, int) {
 
 	topicCnt := len(topics)
 	//创建时间格式2019-09-11T00:16:13.099+0800
-	format := "2006-01-02T15:04:05.000+0800"
+	//format := "2006-01-02T15:04:05.000+0800"
 	//fmt.Printf("len=%v", topicCnt)
-	lastTopicTime, _ := time.Parse(format, topics[topicCnt - 1].Create_time)
+	//lastTopicTime, _ := time.Parse(format, topics[topicCnt - 1].Create_time)
 
-	return lastTopicTime, topicCnt
+
+	return strToTime(topics[topicCnt - 1].Create_time), topicCnt
 }
 
 //从topic文件中获取最后一次拉取topic的create_time，作为lastTopicTime
@@ -447,7 +542,7 @@ func createDir(dir string) {
 }
 
 //拉取指定时间段的topic
-func pullZsxqTopicByPeriod(groupId int, startTime time.Time, endTime time.Time) {
+func pullZsxqTopicByPeriod(groupId int64, startTime time.Time, endTime time.Time) {
 	//创建group目录
 	groupDir := fmt.Sprintf(GROUP_DIR_FMT, groupId)
 	createDir(groupDir)
@@ -481,7 +576,7 @@ func pullZsxqTopicByPeriod(groupId int, startTime time.Time, endTime time.Time) 
 		//注意每次lastTopicTime都需要加一个微小的时间偏移，比如1s
 		topicList := getOnePageTopic(groupId, startTime.Add(-1 * time.Second), PAGE_SIZE)
 		waitGroup.Add(1)
-		go saveTopic(&waitGroup, topicList, topicFile, imageDir)
+		go saveTopic(&waitGroup, groupId, topicList, topicFile, imageDir)
 		//获取最后一个topic的create_time时间作为下次拉取的lastTopicTime，以及page中条数
 		startTime, returnPageSize = getPageInfoFromTopics(topicList)
 
@@ -493,37 +588,92 @@ func pullZsxqTopicByPeriod(groupId int, startTime time.Time, endTime time.Time) 
 	waitGroup.Wait()
 }
 
-//获取已经存在的时间段
-func getExistPeriod(groupId int) (time.Time, time.Time) {
 
+//获取已经存在的时间段
+//existLastestTime 已存在的最新时间
+//existOldTime 已存在的最旧时间
+//如果结果是空time.Time{}, 则使用time.IsZero()来判断即可
+func getExistPeriodFromMysql(groupId int64) (time.Time, time.Time) {
+	fmt.Printf("getExistPeriodFromMysql()")
+	oldTime := time.Time{}
+	lastestTime := time.Time{}
+	var lastestTopics []Topics
+	db.Where("group_id = ?", groupId).Limit(1).Order("topic_time desc").Find(&lastestTopics)
+	cnt := len(lastestTopics)
+	if cnt > 0 {
+		//db有数据
+		//获取最新时间
+		lastestTime = lastestTopics[0].TopicTime
+		var oldTopics []Topics
+		//获取最旧时间
+		db.Where("group_id = ?", groupId).Limit(1).Order("topic_time asc").Find(&oldTopics)
+		oldTime = oldTopics[0].TopicTime
+	}
+
+	return lastestTime, oldTime
+}
+
+
+func getExistPeriodFromFile(groupId int64) (time.Time, time.Time) {
+	//TODO 后续实现
+	return time.Time{}, time.Time{}
+}
+
+//获取已经存在的时间段
+//existLastestTime 已存在的最新时间
+//existOldTime 已存在的最旧时间
+func getExistPeriod(groupId int64) (time.Time, time.Time) {
+
+	if topicSaveType | TOPIC_SAVE_TYPE_BIT_MYSQL != 0 {
+		return getExistPeriodFromMysql(groupId)
+	}
+
+	if topicSaveType | TOPIC_SAVE_TYPE_BIT_FILE != 0 {
+		return getExistPeriodFromFile(groupId)
+	}
+
+	return time.Time{}, time.Time{}
 }
 
 //拉取指定group_id星球的topic
 //应当拉取的时间段，分为头尾2段时间：
 // 1. [当前时间, 已拉取的最新时间]
 // 2. [已拉取的最新时间, 2000-01-01 00:00:00]
-func pullZsxqTopic(groupId int) {
+func pullZsxqTopic(groupId int64) {
 
-	//获取当前已拉取的时间段
-	existStartTime, existEndTime := getExistPeriod(groupId)
-
-	if existStartTime == nil || existEndTime == nil {
-		//未存在已拉取数据，说明需要从now到MOST_LONG_LONG_AGO
-		pullZsxqTopicByPeriod(groupId, time.Now(), strToTime(MOST_LONG_LONG_AGO))
+	if g_startTime != "" && g_endTime != "" {
+		//指定范围拉取
+		pullZsxqTopicByPeriod(groupId, strToTime(g_startTime), strToTime(g_endTime))
 	} else {
-		//有存量数据
-		//1. 拉取前半段
-		pullZsxqTopicByPeriod(groupId, time.Now(), existStartTime)
+		//自动判断拉取范围
+		//获取当前已拉取的时间段
+		existLastestTime, existOldTime := getExistPeriod(groupId)
+		//都统一为东八区
+		//timeNow := strToTime(timeToStr(time.Now(), TIME_ZONE_SHANGHAI), TIME_ZONE_0)
+		if existLastestTime.IsZero() {
+			fmt.Printf("first pull\n")
+			//未存在已拉取数据，说明需要从now到MOST_LONG_LONG_AGO
+			//pullZsxqTopicByPeriod(groupId, timeNow, strToTime(MOST_LONG_LONG_AGO, TIME_ZONE_0))
+			pullZsxqTopicByPeriod(groupId, time.Now(), strToTime(MOST_LONG_LONG_AGO))
+		} else {
+			//fmt.Printf("alread exist period is:[%v, %v]", timeToStr(existLastestTime, TIME_ZONE_0), timeToStr(existOldTime, TIME_ZONE_0))
+			fmt.Printf("alread exist period is:[%v, %v]", timeToStr(existLastestTime), timeToStr(existOldTime))
+			//有存量数据
+			//1. 拉取前半段
+			//pullZsxqTopicByPeriod(groupId, timeNow, existLastestTime)
+			pullZsxqTopicByPeriod(groupId, time.Now(), existLastestTime)
 
-		//2. 拉取后半段
-		pullZsxqTopicByPeriod(groupId, existEndTime, strToTime(MOST_LONG_LONG_AGO))
+			//2. 拉取后半段
+			//pullZsxqTopicByPeriod(groupId, existOldTime, strToTime(MOST_LONG_LONG_AGO, TIME_ZONE_0))
+			pullZsxqTopicByPeriod(groupId, existOldTime, strToTime(MOST_LONG_LONG_AGO))
+		}
 	}
 
 
 
 }
 //拉取指定group_id星球的topic
-//func pullZsxqTopic(groupId int) {
+//func pullZsxqTopic(groupId int64) {
 //	//创建group目录
 //	groupDir := fmt.Sprintf(GROUP_DIR_FMT, groupId)
 //	createDir(groupDir)
@@ -569,10 +719,7 @@ func pullZsxqTopic(groupId int) {
 
 
 func main() {
-
-
-
-
+	flag.Parse()
 
 	//拉取水库 知识星球
 	pullZsxqTopic(GROUP_ID_SHUIKU)
--- test/test.go
+++ test/test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/jinzhu/gorm"
+	"time"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 type Cookie struct {
@@ -18,7 +21,63 @@ type Cookie struct {
 	name string `json:"name"`
 	value string `json:"value"`
 }
+type Topics struct {
+	ID 				int64 		`gorm:"primary_key"`
+	GroupId 		int64		`gorm:"type:bigint(20);not null;unique_index:idx_group_time_id;"`
+	TopicContent 	string 		`gorm:"type:text;not null;"`
+	UpdateAt 		time.Time	`gorm:"type:timestamp"`
+	TopicTime 		time.Time 	`gorm:"type:datetime(3);unique_index:idx_group_time_id;"`
+	CreateAt 		time.Time	`gorm:"type:timestamp"`
+}
+//话题
+type Topic struct {
+	Question Question 	`json:"question"` //提问
+	Answer Answer 		`json:"answer"` //回答
+	Show_comments []ShowComment 	`json:"show_comments"` //评论列表
+	Likes_count int64 	`json:"likes_count"` //点赞数
+	Comments_count int64 	`json:"comments_count"` //评论数
+	Reading_count int64 `json:"reading_count"` //阅读数
+	Create_time string 	`json:"create_time"` //创建时间2019-09-11T00:16:13.099+0800
+}
 
+type Owner struct {
+	User_id uint64 	`json:"user_id"`  //提问者id
+	Name string 	`json:"name"` //提问者名字
+}
+
+type ImageInfo struct {
+	Url string 		`json:"url"`
+	Width uint64 	`json:"width"`
+	Height uint64 	`json:"height"`
+	Size uint64		`json:"size"`
+}
+
+type Image struct {
+	Image_id uint64 	`json:"image_id"`
+	Type string 		`json:"type"`
+	Thumbnail ImageInfo `json:"thumbnail"`
+	Large ImageInfo 	`json:"large"`
+	Original ImageInfo	`json:"original"`
+}
+
+type Question struct {
+	Owner Owner 		`json:"owner"` //提问者
+	Text string 		`json:"text"` //问题内容
+	Images []Image 		`json:"images"` //问题图片
+}
+
+type Answer struct {
+	Owner Owner 	`json:"owner"` //回答者
+	Text string 	`json:"text"` //回答内容
+	Images []Image 		`json:"images"` //回答图片
+}
+
+type ShowComment struct {
+	Comment_id int64 	`json:"comment_id"`
+	Create_time string 	`json:"create_time"`
+	Owner Owner 		`json:"owner"`
+	Text string 		`json:"text"` //评论内容
+}
 //func main() {
 //	cookieStr := `[
 //{
@@ -55,6 +114,36 @@ type Cookie struct {
 //	fmt.Printf("cookies %v", cookies)
 //
 //}
+const TIME_ZONE_SHANGHAI 	= "Asia/Shanghai"
+const TIME_ZONE_0 			= ""
+var db *gorm.DB
+
+func init() {
+	var err error
+	//链接mysql
+	db, err = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/zsxq?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func timeToStr(t time.Time, timeZone string) (string) {
+	format := "2006-01-02T15:04:05.000+0800"
+	//time.ParseInLocation("2006-01-02 15:04:05", t, time.Local)
+	//var cstSh, _ = time.LoadLocation("Asia/Shanghai") //上海
+	//var cstSh, _ = time.LoadLocation(timeZone) //0时区
+	//return t.In(cstSh).Format(format)
+	return t.Format(format)
+}
+
+func strToTime(s string, timeZone string) (time.Time) {
+	format := "2006-01-02T15:04:05.000+0800"
+	t, _ := time.Parse(format, s)
+	//var cst0, _ = time.LoadLocation(timeZone) //0时区
+	//t, _ := time.ParseInLocation(format, s, cst0)
+	return t
+}
 
 
 func main ( ) {
@@ -72,4 +161,30 @@ func main ( ) {
 		fmt.Println ( "error:" , err )
 	}
 	fmt.Printf ( "%+v" , animals )
+
+	testTime := time.Time{}
+
+	fmt.Printf("%v\n", testTime.IsZero())
+
+
+	//timeStr := "2019-09-29T17:02:01.335+0800"
+	//fmt.Printf("time %v\n", timeToStr(strToTime(timeStr)))
+
+	timeStr := timeToStr(time.Now(), TIME_ZONE_0)
+	fmt.Printf("time %v \n", timeStr)
+	timeObj := strToTime(timeStr, TIME_ZONE_0)
+	fmt.Printf("time %v \n", timeToStr(timeObj, TIME_ZONE_0))
+
+	emojiStr := `{"question":{"owner":{"user_id":0,"name":""},"text":"欧神，您好！看到之前有人问“亲戚入股买房”，您说1.债权融资，2.把对方当仇人，写的清清楚楚。请教您：如果入股权，该写清楚哪几样协议？","images":null},"answer":{"owner":{"user_id":225442182841,"name":"yevon_ou"},"text":"最关键二条；\n\n1) 如果想卖，到哪个价位卖，谁说了算\n\n2) 如果想撤股，如何处理\n\n\n\n\n1½) 如果要装修，民宿，维修，谁说了算。合同细则谁能拍板。","images":null},"show_comments":[{"comment_id":554582154854,"create_time":"2017-04-30T15:54:26.871+0800","owner":{"user_id":222211288481,"name":"azorro"},"text":"（1）划清边界；（2）禁止互相购买，一致对外；（3）约定退出条件；（4）调整自我心态，做好吃亏的准备；（5）手续齐全"},{"comment_id":452514851218,"create_time":"2017-04-30T15:54:42.832+0800","owner":{"user_id":222211288481,"name":"azorro"},"text":"血的教训"},{"comment_id":824215258582,"create_time":"2017-04-30T17:25:48.549+0800","owner":{"user_id":455511518428,"name":"信步小园闲庭"},"text":"说的太对了，现在和亲戚一起投的钱退也退不出，只能过桥+抵押贷。"},{"comment_id":141482421222,"create_time":"2017-04-30T17:37:35.336+0800","owner":{"user_id":481418411128,"name":"zhiwen"},"text":"福州大神吗😁"},{"comment_id":824218855222,"create_time":"2017-04-30T22:44:23.781+0800","owner":{"user_id":458511554248,"name":"郑钦文 Enzo"},"text":"GP和LP要分清楚"}],"likes_count":11,"comments_count":5,"reading_count":118,"create_time":"2017-04-30T15:49:11.423+0800"}`
+
+	topic := Topic{}
+
+	json.Unmarshal([]byte(emojiStr), &topic)
+	fmt.Printf("json1 :%v\n", topic)
+	jsonBytes, _ := json.Marshal(topic)
+	fmt.Printf("json2 :%v\n", string(jsonBytes[:]))
+
+	var topics []Topics
+	db.Where("topic_content like ?", "%我们欧粉赚大发咯%").Find(&topics)
+	fmt.Printf("-----topics %v\n", topics)
 }
